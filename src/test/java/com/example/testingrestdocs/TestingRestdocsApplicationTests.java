0939a1a7fd09aef8a91baf8d6b97a75275fa8411
zypherscript <kitti.kthp@gmail.com>
2023-06-09 15:33:43 +0700

    up

---
+++ src/test/java/com/example/testingrestdocs/TestingRestdocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testingrestdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingRestdocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
